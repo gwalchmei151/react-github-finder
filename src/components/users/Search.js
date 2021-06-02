cca0ed2c418fa840e51ade820e5449e73897aea5
Tushar Ismail <gwalchmei@gmail.com>
2021-06-03 01:51:08 +0800

    passing props up and search + clear users from state done

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
   state = {
@@ -7,12 +8,19 @@ class Search extends Component {
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
+  static propType = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  };
   onSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -29,6 +37,11 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
