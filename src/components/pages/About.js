3d724f12763097d9c497ce68b1828ab7554bbf6f
Tushar Ismail <gwalchmei@gmail.com>
2021-06-05 03:47:34 +0800

    React router setup + single user component and data done

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+export const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>App to search Github users</p>
+      <br />
+      <p>A tutorial from Brad Traversy's React Front to Back Udemy Course</p>
+      <p>Version: 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
